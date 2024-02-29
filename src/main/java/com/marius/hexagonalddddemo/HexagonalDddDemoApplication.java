f65e0fe4f47d5cc69e094763bb33c8c7a180521e
marius-danciu-ioan <161754349+marius-danciu-ioan@users.noreply.github.com>
2024-02-29 19:35:21 +0100

    first commit
    initial structure created with spring init

---
+++ src/main/java/com/marius/hexagonalddddemo/HexagonalDddDemoApplication.java
@@ -0,0 +1,13 @@
+package com.marius.hexagonalddddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalDddDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalDddDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marius/hexagonalddddemo/HexagonalDddDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marius.hexagonalddddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalDddDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
